74dfe6dfa6fc6f8ef0c17e3f37fa9b1f78c3e669
Anil Suthar <asuthar2002@gmail.com>
2025-10-15 15:43:55 +0530

    udpate the cors

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const PORT = process.env.PORT;
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://master-o-quizz-back-a.up.railway.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"]
