e2c9762e9377586976fb1cfc89145eb8c8ae2c12
Anil Suthar <asuthar2002@gmail.com>
2025-10-15 15:07:26 +0530

    new

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,11 +2,7 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const sequelize = new Sequelize(
-  process.env.DB_NAME as string,
-  process.env.DB_USER as string,
-  process.env.DB_PASS as string,
-  {
+export const sequelize = new Sequelize(process.env.DB_URL as string,{
     host: process.env.DB_HOST,
     dialect: "mysql",
     port: Number(process.env.DB_PORT) || 3306,
